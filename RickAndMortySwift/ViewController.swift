af78db939582db18e8aadf9c635e20cfab20da3d
PieroAp <pacevedop@proveedor.intercorp.com.pe>
2023-06-08 22:08:57 -0500

    Initial Commit

---
+++ RickAndMortySwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortySwift
+//
+//  Created by Piero Jhoann Acevedo Pichen on 8/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
